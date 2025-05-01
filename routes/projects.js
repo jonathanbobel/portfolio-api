1620b4aaeb1ec6c7d91fc1894b9e097eaa997367
Jonathan Bobel <jonathan.bobel@gsa.gov>
2025-05-01 15:08:58 -0400

    Update projects.js

--- routes/projects.js
+++ routes/projects.js
@@ -3,10 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // Sample project data
-const projects = [
-  { id: 1, name: 'Project 1', description: 'This is project 1', technologies: ['Node.js', 'Express', 'MongoDB'] },
-  { id: 2, name: 'Project 2', description: 'This is project 2', technologies: ['React', 'Node.js', 'PostgreSQL'] },
-];
+const projects = require('../data/projects.json');
 
 // GET all projects
 router.get('/', (req, res) => {
